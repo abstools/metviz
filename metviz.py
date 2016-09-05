6ac7f0a70d7c38ee795cd1d0b154a5756d36d1d2
Behrooz Nobakht <nobeh5@gmail.com>
2016-09-05 09:18:03 +0200

    Add Clock Cycle Factor
    Add option '--ccf' to present the relational factor to clock cycle
    that Python sees.
    
    Signed-off-by: Behrooz Nobakht <nobeh5@gmail.com>

--- metviz.py
+++ metviz.py
@@ -53,7 +53,7 @@ def process_metrics(values):
         # Assume this is proper JSON
         return values
 
-def send_metrics(metrics, t0 = T0, host = "localhost", port = 8086, db = 'datasource'):
+def send_metrics(metrics, t0 = T0, ccf = 1.0, host = "localhost", port = 8086, db = 'datasource'):
     """Send a bag of metrics to a data backend.
 
     metrics -- the dict of metric values
@@ -67,7 +67,7 @@ def send_metrics(metrics, t0 = T0, host = "localhost", port = 8086, db = 'dataso
         m_time, metric_values = list(m.items())[0]
         for measurement in metric_values:
             key, value = list(measurement.items())[0]
-            delta = int(m_time)
+            delta = int(float(m_time) * ccf)
             actual_time = datetime.utcnow() if DEBUG else (t0 + timedelta(milliseconds = delta))
             epoch_nanos = to_nano(actual_time - EPOCH)
             # Protocol: "METRIC[,TAGS] value=VAL TIMESTAMP"
@@ -86,7 +86,7 @@ def to_nano(td):
     """
     return int(1000000000 * td.total_seconds())
 
-def start(func_metrics, metrics_url, interval=9.5, t0=T0, ds_host='localhost', ds_port=8086, ds_db = 'datasource'):
+def start(func_metrics, metrics_url, interval=9.5, t0=T0, ccf=1.0, ds_host='localhost', ds_port=8086, ds_db = 'datasource'):
     """Start the cycle to get metrics, process the metrics,
     and write them to the data source.
 
@@ -99,7 +99,7 @@ def start(func_metrics, metrics_url, interval=9.5, t0=T0, ds_host='localhost', d
     while True:
         raw_metrics = func_metrics(metrics_url)
         metrics = process_metrics(raw_metrics)
-        send_metrics(metrics, t0, ds_host, ds_port, ds_db)
+        send_metrics(metrics, t0, ccf, ds_host, ds_port, ds_db)
         logging.info("Waiting %s for the next round ...", interval)
         time.sleep(interval)
 
@@ -114,6 +114,8 @@ def args_parser():
         help = "The interval (seconds) between each cycle")
     parser.add_argument('--t0', required = False,
         help = "The time of the start of the system for data backend. All the time should be in UTC time zone. If not provided, UTC `now()` is used as the reference. The format to provide this time is 'Y-m-d_H-M-S'; e.g. '2001-03-05_06-12-09'.")
+    parser.add_argument('--ccf', required = False, default = "1.0",
+        help = "Clock Cycle Factor. A double value in (0, 1) to denote the clock cycles factor in relation with real-time clock. A value of 1 means the same clock cycle factor as Python 'time' module sees.")
     parser.add_argument('--host', default = 'localhost', required = False,
         help = "The host of the data backend")
     parser.add_argument('--port', default = '8086', required = False,
@@ -131,10 +133,11 @@ def main(args):
     func_get_metrics = get_metrics_example if metrics_url == "TEST" else get_metrics
     interval = float(args.interval)
     t0 = datetime.strptime(args.t0, '%Y-%m-%d_%H-%M-%S') if args.t0 != None else datetime.utcnow()
+    ccf = float(args.ccf)
     ds_host = args.host
     ds_port = int(args.port)
     ds_db = args.database
-    start(func_get_metrics, metrics_url, interval, t0, ds_host, ds_port, ds_db)
+    start(func_get_metrics, metrics_url, interval, t0, ccf, ds_host, ds_port, ds_db)
 
 if __name__ == "__main__":
     logging.basicConfig(
